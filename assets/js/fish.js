e00d2bfc0922cbb1798cef9bdee2899b478be446
Yumi Kim <yumikimsk@gmail.com>
2016-03-07 16:34:38 -0500

    save fish amout in local storage and don't increment fish on new page load

--- assets/js/fish.js
+++ assets/js/fish.js
@@ -2,12 +2,26 @@ var fishContainer;
 var fish;
 
 $( document ).ready(function() {
+
     fishContainer = $('#money-counter');
-    fish = fishContainer.html();
+
+    // grab fish value from local storage
+    if (localStorage.getItem('fish')) {
+        fish = localStorage.getItem('fish');
+    } else {
+        fish = 0;
+    }
+
+    // set incrementFish() to run every second
+    incrementFish(false);
     setInterval(incrementFish, 1000);
 });
 
-function incrementFish () {
-    fish++;
+function incrementFish (bool) {
+    if (bool || bool == null) {
+        fish++;
+    }
+
     fishContainer.html(fish);
+    localStorage.setItem('fish',fish);
 }
