b964c7f7e66c8ab2b2b65d37cf5f6ff46346faf9
Yumi Kim <yumikimsk@gmail.com>
2016-03-07 16:29:21 -0500

    increment fish based on time

--- assets/js/fish.js
+++ assets/js/fish.js
@@ -1,1 +1,13 @@
-console.log("hello, world");
+var fishContainer;
+var fish;
+
+$( document ).ready(function() {
+    fishContainer = $('#money-counter');
+    fish = fishContainer.html();
+    setInterval(incrementFish, 1000);
+});
+
+function incrementFish () {
+    fish++;
+    fishContainer.html(fish);
+}
