44a1179ce45aa002fea3861eef31768e7bc867c1
Yumi Kim <yumikimsk@gmail.com>
2016-03-10 00:19:07 -0500

    adjust items

--- assets/js/items.js
+++ assets/js/items.js
@@ -1,16 +1,17 @@
 var items = [
-    new item("leaf",5,false),
-    new item("bait",10,false),
-    new item("squid",25,false),
-    new item("catnip",50,false),
-    new item("fishing-rod",100,false),
-    new item("scratching-post",200,false),
-    new item("bed",500,false),
-    new item("header",1000,false)
+    new item("leaf",5),
+    new item("bait",10),
+    new item("dry-kibble",25),
+    new item("catnip",50),
+    new item("canned-tune",75),
+    new item("fishing-rod",100),
+    new item("toy",250),
+    new item("scratching-post",500),
+    new item("bed",1000)
 ];
 
-function item(n,c,b) {
+function item(n,c) {
     this.name = n;
     this.cost = c;
-    this.buyable = b;
+    this.buyable = false;
 }
